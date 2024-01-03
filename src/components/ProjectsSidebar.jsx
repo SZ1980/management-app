69ffcbf30fb6ecc0883dbd7a0942efee8b69f11d
Zoran Simonovic <simonovic1980@gmail.com>
2024-01-03 21:05:49 +0100

    styling buttons input

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function ProjectsSidebar() {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
@@ -7,7 +5,7 @@ export default function ProjectsSidebar() {
         Your Projects
       </h2>
       <div>
-        <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
           + Add Project
         </button>
       </div>
